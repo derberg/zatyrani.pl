dbf3538c3fc76f39b15e7b8a3423d2ef4417f327
Lukasz Gornicki <lpgornicki@gmail.com>
2025-05-08 18:27:06 +0200

    feat: final fix to training add and make trainings visible

---
+++ src/data/trainings.ts
@@ -0,0 +1,14 @@
+import trainingsData from './trainings.json';
+
+export interface Training {
+type: 'bieg' | 'nordic walking';
+datetime: string;
+location: string;
+locationLink: string;
+comment?: string;
+phone: string;
+distance: number;
+pace: number;
+}
+
+export const trainings: Training[] = trainingsData as Training[];
