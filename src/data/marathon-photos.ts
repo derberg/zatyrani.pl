f6eac5894d06323d1d6060466ef7356c4664ea9b
FranekPraktyki <franek1285@gmail.com>
2025-11-03 09:53:04 +0100

    chore: fixing the website

--- src/data/marathon-photos.ts
+++ src/data/marathon-photos.ts
@@ -36,3 +36,42 @@ export const marathonPhotos2024 = [
 		alt: "Biegaczka bez psa",
 	},
 ];
+
+export const marathonPhotos2025 = [
+	{
+		src: "/halfmarathon/2025/1.webp",
+		alt: "Radośni Zatyrani",
+	},
+	{
+		src: "/halfmarathon/2025/2.webp",
+		alt: "Radośa Orlica",
+	},
+	{
+		src: "/halfmarathon/2025/3.webp",
+		alt: "Radosny biegacz z Chin",
+	},
+	{
+		src: "/halfmarathon/2025/4.webp",
+		alt: "Radosny biegacz z psem",
+	},
+	{
+		src: "/halfmarathon/2025/5.webp",
+		alt: "Biegacze idą łeb w łeb",
+	},
+	{
+		src: "/halfmarathon/2025/6.webp",
+		alt: "Przyczajona Zatyrana z słuchawkami",
+	},
+	{
+		src: "/halfmarathon/2025/7.webp",
+		alt: "Pijany biegacz",
+	},
+	{
+		src: "/halfmarathon/2025/8.webp",
+		alt: "Biegaczka z psem",
+	},
+	{
+		src: "/halfmarathon/2025/9.webp",
+		alt: "Biegaczka bez psa",
+	},
+];
