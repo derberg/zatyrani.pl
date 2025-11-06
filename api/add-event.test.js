d957df79e9e78b7b05ba2f2270306750393a4740
FranekPraktyki <franek1285@gmail.com>
2025-11-06 11:19:00 +0100

    chore: fix the website

--- api/add-event.test.js
+++ api/add-event.test.js
@@ -1,5 +1,6 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-import { readExistingEventsData, generateUID, formatDate, normalizeEventData } from '../src/utils/events.js';
+import { readExistingEventsData } from '../src/utils/events.js';
+import { generateUID, formatDate, normalizeEventData } from './add-event.js';
 
 describe('readExistingEventsData', () => {
 	let mockOctokit;
