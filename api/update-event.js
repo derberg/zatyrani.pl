aa085c6c7530020b09109ee0fc0b8e9989306b2d
FranekPraktyki <franek1285@gmail.com>
2025-11-07 14:10:34 +0100

    chore: write tests for updateEventByUid function

--- api/update-event.js
+++ api/update-event.js
@@ -4,46 +4,36 @@ import { readExistingEventsData, updateEventsFile } from "../src/utils/events.js
 
 const ExtendedOctokit = Octokit.plugin(createOrUpdateTextFile);
 
+export function updateEventByUid(dataForChange, events, uid) {
+  // Find the index of the event to update
+  const eventIndex = events.findIndex(event => event.uid === uid);
+
+  // Update the event data
+  events[eventIndex] = {
+    ...events[eventIndex], // Keep existing fields like image
+    ...dataForChange
+  };
+  return events;
+}
+
 export default async function handler(req, res) {
   if (req.method !== "PUT") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
   const { uid, name, date, website, registration, description, location } = req.body;
-
-  if (!uid) {
-    return res.status(400).json({ error: "Event UID is missing" });
-  }
-
+  const dataForChange = { name, date, website, registration, description, location };
   try {
     const octokit = new ExtendedOctokit({
       auth: process.env.GITHUB_TOKEN,
     });
 
     let events = await readExistingEventsData(octokit);
-
-    // Find the index of the event to update
-    const eventIndex = events.findIndex(event => event.uid === uid);
-
-    if (eventIndex === -1) {
-      return res.status(404).json({ error: "Event not found" });
-    }
-
-    // Update the event data
-    events[eventIndex] = {
-      ...events[eventIndex], // Keep existing fields like image
-      title: name,
-      date: date, // Assuming date is already formatted as DD/MM/YYYY
-      location: location,
-      description: description,
-      mainLink: website,
-      registrationLink: registration,
-    };
-
+    const updatedEvents = updateEventByUid(dataForChange, events, uid);;
     await updateEventsFile(
       octokit,
       `chore(events): updated event with UID ${uid}`,
-      events
+      updatedEvents
     );
 
     res.status(200).json({ success: true, message: `Event with UID ${uid} updated successfully.` });
