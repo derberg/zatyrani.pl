58ff1fd276158820d2589f3bda9b934f4e18e159
FranekPraktyki <franek1285@gmail.com>
2025-11-18 12:22:50 +0100

    middleware

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { defineMiddleware } from 'astro:middleware';
-
-export const onRequest = defineMiddleware(async (context, next) => {
-  // Dummy middleware for static mode - does nothing
-  return next();
-});
