a57eef3ee047a0f1c024fc98028e70ef7bcf15da
Lukasz Gornicki <lpgornicki@gmail.com>
2026-02-04 20:40:33 +0100

    fix: remove data picker from vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
 
 export default defineConfig({
   integrations: [
-    tailwind(),
-    vue()
+    tailwind()
   ],
   trailingSlash: "never",
 });
