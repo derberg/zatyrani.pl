88c607ad2f04c9fc4086f09e3cb34359497eaa03
Lukasz Gornicki <lpgornicki@gmail.com>
2026-03-15 18:08:23 +0100

    feat: add new rajd 2026

--- src/data/rajdnw-photos.ts
+++ src/data/rajdnw-photos.ts
@@ -1,19 +1,32 @@
-export const rajdnwPhotos2024 = [
+export const rajdnwPhotos2025 = [
   {
-    src: "/rajdnw/2024/1.webp",
-    alt: "Radośni Zatyrani",
+    src: "/rajdnw/2025/4.webp",
+    alt: "Rajd Nordic Walking 2025",
+    featured: true,
   },
   {
-    src: "/rajdnw/2024/2.webp",
-    alt: "Radośa Orlica",
+    src: "/rajdnw/2025/1.webp",
+    alt: "Rajd Nordic Walking 2025",
+  },
+  {
+    src: "/rajdnw/2025/2.webp",
+    alt: "Rajd Nordic Walking 2025",
+  },
+  {
+    src: "/rajdnw/2025/3.webp",
+    alt: "Rajd Nordic Walking 2025",
+  },
+];
+
+export const rajdnwPhotos2024 = [
+  {
+    src: "/rajdnw/2024/4.webp",
+    alt: "Radosny biegacz z psem",
+    featured: true,
   },
   {
     src: "/rajdnw/2024/3.webp",
     alt: "Radosny biegacz z Chin",
-  },
-  {
-    src: "/rajdnw/2024/4.webp",
-    alt: "Radosny biegacz z psem",
   },
   {
     src: "/rajdnw/2024/5.webp",
